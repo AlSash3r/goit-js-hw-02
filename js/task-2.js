9fffba1e1d512258b9e9b513210c64163b700a73
Olexandra Ghordievska <maynarisochka@gmail.com>
2024-10-02 20:40:01 +0300

    add markup 3

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 function formatMessage(message, maxLength) {
-  return message.length <= maxLength 
-    ? message 
-    : message.slice(0, maxLength) + '...';
+  return message.length <= maxLength
+    ? message
+    : message.slice(0, maxLength) + "...";
 }
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
